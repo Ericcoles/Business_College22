cd81e1f15874439b35e83cf38e51a500a282e7df
Coles Eric <s2200118@BC-C02GR0BJQ6LR-MacBook-Air.local>
2022-02-13 22:05:44 +0200

    Portfolio Update

--- Marg/JS_Project/4_week/10_2_2022/main.js
+++ Marg/JS_Project/4_week/10_2_2022/main.js
@@ -14,8 +14,76 @@
 //     return this.age;
 //   },
 // };
+///////////////////
+
+// Marg
+// let formInput = document.querySelector("#inputData");
+// let resultTable = document.querySelector(".result");
+
+// class Car {
+//   constructor(licence, maker, model, owner, price, color) {
+//     this.licence = licence;
+//     this.maker = maker;
+//     this.model = model;
+//     this.owner = owner;
+//     this.price = price;
+//     this.color = color;
+//   }
+// }
+// const carArray = [];
+
+// const addCar = (event) => {
+//   event.preventDefault();
+//   let licence = document.querySelector("#licence").value;
+//   let maker = document.querySelector("#maker").value;
+//   let model = document.querySelector("#model").value;
+//   let owner = document.querySelector("#owner").value;
+//   // let price = Number(document.querySelector("#price")).value;
+//   let price = document.querySelector("#price").value;
+//   let color = document.querySelector("#color").value;
+
+//   let newCar = new Car(licence, maker, model, owner, price, color);
+
+//   carArray.push(newCar);
+
+//   console.table(carArray);
+
+//   console.log(
+//     "form was submitted",
+//     licence,
+//     maker,
+//     owner,
+//     model,
+//     +price,
+//     color
+//   );
+// };
+
+// const makeTable = () => {
+//   let result = "<table border=1 >";
+//   result += "<tr>";
+//   for (const key in carArray[0]) {
+//     result += "<th>" + key + "</th>";
+//   }
+//   for (let i = i; i < carArray.length; i++) {
+//     result += "<tr>";
+//     for (const key in carArray[i]) {
+//       result += "<td>" + carArray[i][key] + "</td>";
+//     }
+//     result += "</tr>";
+//   }
+//   result += "</table>";
+//   resultTable.innerHTML = result;
+//   console.log(result);
+// };
+
+// makeTable();
+
+// formInput.addEventListener("submit", addCar);
+////// Straight from github below
 
 let formInput = document.querySelector("#inputData");
+let resultTable = document.querySelector(".result");
 
 class Car {
   constructor(licence, maker, model, owner, price, color) {
@@ -27,7 +95,9 @@ class Car {
     this.color = color;
   }
 }
-const carArray = [];
+const carArray = [
+  { licence: "", maker: "", model: "", owner: "", price: "", color: "" },
+];
 
 const addCar = (event) => {
   event.preventDefault();
@@ -35,26 +105,36 @@ const addCar = (event) => {
   let maker = document.querySelector("#maker").value;
   let model = document.querySelector("#model").value;
   let owner = document.querySelector("#owner").value;
-  // let price = Number(document.querySelector("#price")).value;
-  let price = document.querySelector("#price").value;
-
+  let price = Number(document.querySelector("#price").value);
   let color = document.querySelector("#color").value;
 
   let newCar = new Car(licence, maker, model, owner, price, color);
 
   carArray.push(newCar);
-
   console.table(carArray);
-
-  console.log(
-    "form was submitted",
-    licence,
-    maker,
-    owner,
-    model,
-    +price,
-    color
-  );
+  console.log("form was submitted", licence, maker, model, owner, price, color);
+  makeTable();
 };
+
+const makeTable = () => {
+  let result = "<table border=1 >";
+  result += "<tr>";
+  for (const key in carArray[0]) {
+    result += "<th>" + key + "</th>";
+  }
+  result += "</tr>";
+  for (let i = 1; i < carArray.length; i++) {
+    result += "<tr>";
+    for (const key in carArray[i]) {
+      result += "<td>" + carArray[i][key] + "</td>";
+    }
+    result += "</tr>";
+  }
+  result += "</table>";
+  resultTable.innerHTML = result;
+  console.log(result);
+};
+
+makeTable();
 
 formInput.addEventListener("submit", addCar);
