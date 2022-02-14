ca0e4ce74849e11a6a58843701d17f28447fe192
Coles Eric <s2200118@BC-C02GR0BJQ6LR-MacBook-Air.local>
2022-02-14 21:24:04 +0200

    Santosh 14 Feb

---
+++ Santoshi/Javascript/5_week/Practice_lessons11402/classes/class3.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/**
+Write the Recipe class such that it automatically console logs the string: New recipe created whenever
+we create a new instance of that class.
+**/
+
+class Recipe {}
+let Recipe1 = new Recipe();
+// let bread = new Recipe("Egg", "Flour", "Baking Soda", "Salt");
